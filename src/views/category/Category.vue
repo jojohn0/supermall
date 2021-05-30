11d375dda9d8641140ff0dbcf77126582899037e
jojohn0 <2351061127@qq.com>
2021-05-30 08:51:26 +0800

    测试better-scorll

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,12 +1,95 @@
 <template>
-  <h2>分类</h2>
+  <div class="wrapper">
+    <ul class="content">
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import BScroll from "better-scroll";
 export default {
   name: "Category",
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  created() {},
+  mounted() {
+    this.scroll = new BScroll(document.querySelector(".wrapper"), {});
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.wrapper {
+  height: 150px;
+  background-color: red;
+}
 </style>
